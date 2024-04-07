b061fe6a79ab435413096c458eed52e52da82437
Pythom1234 <149593207+Pythom1234@users.noreply.github.com>
2024-04-07 12:09:59 +0200

    Aktualizovat extension.ts

--- extension.ts
+++ extension.ts
@@ -2,8 +2,8 @@ namespace pokus {
     /**
      * Help goes here.
      */
-    //% shim=pokus::extfun
-    export function extfun(x: number, y: number) {
+    //% shim=pokus::pokus
+    export function pokus(x: number, y: number) {
         // Dummy implementation for the simulator.
         return x - y
     }
