d7edcd166bfeab2a4b3f92f7f001016e96003a3d
Pythom1234 <149593207+Pythom1234@users.noreply.github.com>
2024-04-07 12:09:41 +0200

    Obnovit '7. 4. 2024 12:08:25 Aktualizovat extension.cpp'

--- extension.ts
+++ extension.ts
@@ -1,2 +1,10 @@
 namespace pokus {
+    /**
+     * Help goes here.
+     */
+    //% shim=pokus::extfun
+    export function extfun(x: number, y: number) {
+        // Dummy implementation for the simulator.
+        return x - y
+    }
 }
