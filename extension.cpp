c3fec18673a173cf03f632416c26753388bc87a1
Pythom1234 <149593207+Pythom1234@users.noreply.github.com>
2024-05-12 12:15:39 +0200

    Aktualizovat extension.cpp

--- extension.cpp
+++ extension.cpp
@@ -6,8 +6,8 @@ namespace sound
 	int record()
     {
         uBit.init();
-        uBit.io.runmic.setDigitalValue(1);
-        uBit.io.runmic.setHighDrive(true);
-        return uBit.io.microphone.getAnalogValue();
+        uBit.io.pin[20].setDigitalValue(1);
+        uBit.io.pin[20].setHighDrive(true);
+        return uBit.io.pin[5].getAnalogValue();
 	}
 }
