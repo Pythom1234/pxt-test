a90389232701f98540bd6a02cecddaf4638b3fa0
Pythom1234 <149593207+Pythom1234@users.noreply.github.com>
2024-05-12 12:26:35 +0200

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -1,2 +1,10 @@
 namespace sound {
+    //% block
+    //% shim=sound::record
+    export function record(): number {
+        return 0
+    }
+    //% block
+    //% shim=sound::init
+    export function init(): void {}
 }
